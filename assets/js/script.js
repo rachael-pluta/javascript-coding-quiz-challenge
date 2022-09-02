f7fb9ee3451a556e5833cf8bc9529bc0648e0f58
rachael-pluta <rachael.pluta.work@gmail.com>
2022-09-02 13:17:25 +1000

    included randomised question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,78 @@
-var startQuiz() = document.getElementById('start-quiz')
+var startButton = document.getElementById('start-quiz')
 
+var randomisedQuestions, currentQuestion
+
+startButton.addEventListener('click', startQuiz)
+
+/*
 // Sets the questions and answers to the quiz
-var Questions =[ {
-    id = 0,
+var questions = [ 
+    {
     q: "Arrays in JavaScript can be used to store: _________.",
-    a: [{text: "a. numbers and strings", isCorrect = false},
+    a: [
+        {text: "a. numbers and strings", isCorrect = false},
         {text: "b. other arrays", isCorrect = false},
         {text: "c. booleans", isCorrect = false},
         {text: "d. all of the above", isCorrect = true}
     ]
 },
 {
-    id = 1,
     q: "String values must be enclosed within ______ when being assigned to variables.",
-    a: [{text: "a. commas", isCorrect = false},
+    a: [
+        {text: "a. commas", isCorrect = false},
         {text: "b. curly brackets", isCorrect = false},
         {text: "c. quotes", isCorrect = true},
         {text: "d. parenthesis", isCorrect = false},
     ]
 },
 {
-    id = 2,
     q: "Commonly used data types do NOT include: ___________.",
-    a: [{text: "a. strings", isCorrect = false},
+    a: [
+        {text: "a. strings", isCorrect = false},
         {text: "b. booleans", isCorrect = false},
         {text: "c. alerts", isCorrect = true},
         {text: "d. numbers", isCorrect = false},
 ]
 },
 {
-    id = 3,
     q: "The condition in an if / else statement is enclosed with: _________.",
-    a: [{text: "a. quotes", isCorrect = },
+    a: [
+        {text: "a. quotes", isCorrect = },
         {text: "b. curly brackets", isCorrect = },
         {text: "c. parenthesis", isCorrect = },
         {text: "d. square brackets", isCorrect = },
 ]
 },
 {
-    id = 4,
     q: "A very useful tool used during development and debugging for printing content to the debugger is: ________.",
-    a: [{text: "a. JavaScript", isCorrect = false},
+    a: [
+        {text: "a. JavaScript", isCorrect = false},
         {text: "b. terminal/bash", isCorrect = false},
         {text: "c. for loops", isCorrect = false},
         {text: "d. console.log", isCorrect = true},
 ]
-},
-{
-    id = 5,
-    q: ""
 }
 ]
 
-function startQuiz(){
+*/
 
+function startQuiz() {
+    console.log('Started Quiz')
+    randomisedQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestion = 0
+    // include element hiding start button here
+    // include element displaying question container here
+    setQuestion()
 }
 
-function displayQuestion(){
-
+function setQuestion() {
+    displayQuestion(randomisedQuestions[currentQuestion])
 }
 
-function displayAnswer(){
+function displayQuestion(question){
     
+}
+
+function displayAnswer() {
+
 }
