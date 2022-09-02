223de101bd891747323f5bafa9f40654be180445
rachael-pluta <rachael.pluta.work@gmail.com>
2022-09-02 12:27:42 +1000

    setup html, css and javascript files, and set out quiz questions in javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+
+// Sets the questions and answers to the quiz
+var Questions =[ {
+    id = 0,
+    q: "Arrays in JavaScript can be used to store: _________.",
+    a: [{text: "a. numbers and strings", isCorrect = false},
+        {text: "b. other arrays", isCorrect = false},
+        {text: "c. booleans", isCorrect = false},
+        {text: "d. all of the above", isCorrect = true}
+    ]
+},
+{
+    id = 1,
+    q: "String values must be enclosed within ______ when being assigned to variables.",
+    a: [{text: "a. commas", isCorrect = false},
+        {text: "b. curly brackets", isCorrect = false},
+        {text: "c. quotes", isCorrect = true},
+        {text: "d. parenthesis", isCorrect = false},
+    ]
+},
+{
+    id = 2,
+    q: "Commonly used data types do NOT include: ___________.",
+    a: [{text: "a. strings", isCorrect = false},
+        {text: "b. booleans", isCorrect = false},
+        {text: "c. alerts", isCorrect = true},
+        {text: "d. numbers", isCorrect = false},
+]
+},
+{
+    id = 3,
+    q: "The condition in an if / else statement is enclosed with: _________.",
+    a: [{text: "a. quotes", isCorrect = },
+        {text: "b. curly brackets", isCorrect = },
+        {text: "c. parenthesis", isCorrect = },
+        {text: "d. square brackets", isCorrect = },
+]
+},
+{
+    id = 4,
+    q: "A very useful tool used during development and debugging for printing content to the debugger is: ________.",
+    a: [{text: "a. JavaScript", isCorrect = false},
+        {text: "b. terminal/bash", isCorrect = false},
+        {text: "c. for loops", isCorrect = false},
+        {text: "d. console.log", isCorrect = true},
+]
+},
+{
+    id = 5,
+    q: ""
+}
+
+]
